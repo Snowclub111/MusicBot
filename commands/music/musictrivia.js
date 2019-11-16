09091e92d6a968fa6d82e2d3485d905d01e417e1
Snowclub111 <57830261+Snowclub111@users.noreply.github.com>
2019-11-16 17:18:04 +0700

    Add files via upload

---
+++ commands/music/musictrivia.js
@@ -0,0 +1,234 @@
+const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
+const ytdl = require('ytdl-core');
+const fs = require('fs');
+
+module.exports = class MusicTriviaCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'music-trivia',
+      memberName: 'music-trivia',
+      group: 'music',
+      description: "Engage in a 2000's music quiz with your friends!",
+      guildOnly: true,
+      clientPermissions: ['SPEAK', 'CONNECT'],
+      throttling: {
+        usages: 1,
+        duration: 10
+      }
+    });
+  }
+  async run(message) {
+    // check if user is in a voice channel
+    var voiceChannel = message.member.voice.channel;
+    if (!voiceChannel)
+      return message.say('Please join a voice channel and try again');
+    if (message.guild.musicData.isPlaying === true)
+      return message.channel.send('A trivia or a song is already running');
+    message.guild.musicData.isPlaying = true;
+    message.guild.triviaData.isTriviaRunning = true;
+    // fetch link array from txt file
+    const jsonSongs = fs.readFileSync(
+      'resources/music/musictrivia.json',
+      'utf8'
+    );
+    var videoDataArray = JSON.parse(jsonSongs).songs;
+    // get random x videos from array
+    const numOfLinks = 5;
+    const randomXVideoLinks = this.getRandom(videoDataArray, numOfLinks); // get x random urls
+    // create and send info embed
+    const infoEmbed = new MessageEmbed()
+      .setColor('#ff7373')
+      .setTitle('Starting Music Quiz')
+      .setDescription(
+        `Get ready! There are ${numOfLinks} songs, you have 30 seconds to guess either the singer/band or the name of the song. Good luck!`
+      );
+    message.say(infoEmbed);
+    // init quiz queue
+    // turn each vid to song object
+
+    for (let i = 0; i < randomXVideoLinks.length; i++) {
+      const song = {
+        url: randomXVideoLinks[i].url,
+        singer: randomXVideoLinks[i].singer,
+        title: randomXVideoLinks[i].title,
+        voiceChannel
+      };
+      message.guild.triviaData.triviaQueue.push(song);
+    }
+    const channelInfo = Array.from(
+      message.member.voice.channel.members.entries()
+    );
+    channelInfo.forEach(user => {
+      message.guild.triviaData.triviaScore.set(user[1].user.username, {
+        score: 0
+      });
+    });
+    this.playQuizSong(message.guild.triviaData.triviaQueue, message);
+  }
+
+  playQuizSong(queue, message) {
+    queue[0].voiceChannel.join().then(connection => {
+      const dispatcher = connection
+        .play(
+          ytdl(queue[0].url, {
+            quality: 'highestaudio',
+            highWaterMark: 1024 * 1024 * 1024
+          })
+        )
+        .on('start', () => {
+          message.guild.musicData.songDispatcher = dispatcher;
+          let songNameFound = false;
+          let songSingerFound = false;
+
+          const filter = m =>
+            message.guild.triviaData.triviaScore.has(m.author.username);
+          const collector = message.channel.createMessageCollector(filter, {
+            time: 30000
+          });
+
+          collector.on('collect', m => {
+            if (!message.guild.triviaData.triviaScore.has(m.author.username))
+              return;
+            if (m.content.startsWith(this.client.commandPrefix)) return;
+            // if user guessed song name
+            if (m.content.toLowerCase() === queue[0].title) {
+              if (songNameFound) return; // if song name already found
+              songNameFound = true;
+
+              if (songNameFound && songSingerFound) {
+                message.guild.triviaData.triviaScore.get(m.author.username)
+                  .score++;
+                m.react('☑');
+                return collector.stop();
+              }
+              message.guild.triviaData.triviaScore.get(m.author.username)
+                .score++;
+              m.react('☑');
+            }
+            // if user guessed singer
+            else if (m.content.toLowerCase() === queue[0].singer) {
+              if (songSingerFound) return;
+              songSingerFound = true;
+              if (songNameFound && songSingerFound) {
+                message.guild.triviaData.triviaScore.get(m.author.username)
+                  .score++;
+                m.react('☑');
+                return collector.stop();
+              }
+
+              message.guild.triviaData.triviaScore.get(m.author.username)
+                .score++;
+              m.react('☑');
+            } else if (
+              m.content.toLowerCase() ===
+                queue[0].singer + ' ' + queue[0].title ||
+              m.content.toLowerCase() === queue[0].title + ' ' + queue[0].singer
+            ) {
+              if (
+                (songSingerFound && !songNameFound) ||
+                (songNameFound && !songSingerFound)
+              ) {
+                message.guild.triviaData.triviaScore.get(m.author.username)
+                  .score++;
+                m.react('☑');
+                return collector.stop();
+              }
+              message.guild.triviaData.triviaScore.get(
+                m.author.username
+              ).score =
+                message.guild.triviaData.triviaScore.get(m.author.username)
+                  .score + 2;
+              m.react('☑');
+              return collector.stop();
+            } else {
+              // wrong answer
+              return m.react('❌');
+            }
+          });
+
+          collector.on('end', () => {
+            console.log(
+              Array.from(message.guild.triviaData.triviaScore.entries())
+            );
+            message.channel.send(
+              this.scoreEmbed(
+                Array.from(message.guild.triviaData.triviaScore.entries())
+              )
+            );
+            queue.shift();
+            dispatcher.end();
+            return;
+          });
+        })
+        .on('end', () => {
+          if (queue.length >= 1) {
+            return this.playQuizSong(queue, message);
+          } else {
+            if (message.guild.triviaData.wasTriviaEndCalled) {
+              message.guild.musicData.isPlaying = false;
+              message.guild.triviaData.isTriviaRunning = false;
+              message.guild.triviaData.wasTriviaEndCalled = false;
+              message.guild.triviaData.triviaScore.clear();
+              message.guild.me.voice.channel.leave();
+              return;
+            }
+            let highestTriviaScore = 0;
+            let winner = '';
+            let isHighestValueDuplicate = false;
+
+            Array.from(message.guild.triviaData.triviaScore.entries()).map(
+              entry => {
+                if (entry[1].score > highestTriviaScore) {
+                  highestTriviaScore = entry[1].score;
+                  winner = entry[0];
+                  isHighestValueDuplicate = false;
+                } else if (entry[1].score == highestTriviaScore) {
+                  isHighestValueDuplicate = true;
+                }
+              }
+            );
+            if (highestTriviaScore == 0 || isHighestValueDuplicate)
+              return message.channel.send('No one won. Better luck next time');
+            message.channel.send(
+              `The winner is ${winner} with ${highestTriviaScore} points`
+            );
+            message.guild.musicData.isPlaying = false;
+            message.guild.triviaData.isTriviaRunning = false;
+            message.guild.triviaData.triviaScore.clear();
+            message.guild.me.voice.channel.leave();
+            return;
+          }
+        });
+    });
+  }
+
+  getRandom(arr, n) {
+    var result = new Array(n),
+      len = arr.length,
+      taken = new Array(len);
+    if (n > len)
+      throw new RangeError('getRandom: more elements taken than available');
+    while (n--) {
+      var x = Math.floor(Math.random() * len);
+      result[n] = arr[x in taken ? taken[x] : x];
+      taken[x] = --len in taken ? taken[len] : len;
+    }
+    return result;
+  }
+
+  scoreEmbed(arr) {
+    if (!arr) return;
+
+    // create an embed with no fields
+    const embed = new MessageEmbed()
+      .setColor('#ff7373')
+      .setTitle('Trivia Score');
+
+    for (let i = 0; i < arr.length; i++) {
+      embed.addField(arr[i][0] + ':', arr[i][1].score);
+    }
+
+    return embed;
+  }
+};
---
+++ commands/other/reddit.js
@@ -0,0 +1,112 @@
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+const { Command } = require('discord.js-commando');
+
+module.exports = class RedditCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'reddit',
+      aliases: ['subreddit', 'reddit-search'],
+      group: 'other',
+      memberName: 'reddit',
+      description:
+        'Replies with 5 top daily posts in wanted subreddit, you can specify sorting and time',
+      throttling: {
+        usages: 2,
+        duration: 10
+      },
+      args: [
+        {
+          key: 'subreddit',
+          prompt: 'What subreddit would you like to search?',
+          type: 'string',
+          default: 'all',
+          max: 50,
+          wait: 20
+        },
+        {
+          key: 'sort',
+          prompt:
+            'What posts do you want to see? Choose from best/hot/top/new/controversial/rising',
+          type: 'string',
+          default: 'top',
+          validate: sort =>
+            sort === 'best' ||
+            sort === 'hot' ||
+            sort === 'new' ||
+            sort === 'top' ||
+            sort === 'controversial' ||
+            sort === 'rising',
+          wait: 10
+        }
+      ]
+    });
+  }
+
+  // If you want to restrict nsfw posts, remove the commented out code below
+
+  async run(message, { subreddit, sort }) {
+    if (sort === 'top' || sort === 'controversial') {
+      await message.say(
+        `Do you want to get the ${sort} posts from past hour/week/month/year or all?`
+      );
+      try {
+        var t = await message.channel.awaitMessages(
+          m =>
+            m.content === 'hour' ||
+            m.content === 'week' ||
+            m.content === 'month' ||
+            m.content === 'year' ||
+            m.content === 'all',
+          {
+            max: 1,
+            maxProcessed: 1,
+            time: 60000,
+            errors: ['time']
+          }
+        );
+        var timeFilter = t.first().content;
+      } catch (e) {
+        console.error(e);
+        return message.say('Please try again and enter a proper time filter');
+      }
+    }
+    fetch(
+      `https://www.reddit.com/r/${subreddit}/${sort}/.json?limit=5&t=${
+        timeFilter ? timeFilter : 'day'
+      }`
+    )
+      .then(res => res.json())
+      .then(json => {
+        const dataArr = json.data.children;
+        for (let i = 0; i < dataArr.length; i++) {
+          // if (dataArr[i].data.over_18 === true) {
+          //   message.say(':no_entry: nsfw :no_entry:');
+          // } else {
+          message.say(embedPost(dataArr[i].data));
+          //}
+        }
+      })
+      .catch(e => {
+        message.say('The subreddit you asked for was not found');
+        return console.error(e);
+      });
+    // returns an embed that is ready to be sent
+    function embedPost(data) {
+      if (data.title.length > 255) {
+        data.title = data.title.substring(0, 252) + '...'; // discord.js does not allow embed title lengths greater than 256
+      }
+      return new MessageEmbed()
+        .setColor(data.over_18 ? '#cf000f' : '#FE9004') // if post is nsfw, color is red
+        .setTitle(data.title)
+        .setThumbnail(
+          data.thumbnail === 'self'
+            ? 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Reddit.svg/256px-Reddit.svg.png'
+            : data.thumbnail
+        )
+        .setURL(`https://www.reddit.com${data.permalink}`)
+        .setDescription(`Upvotes: ${data.score} :thumbsup: `)
+        .setAuthor(data.author);
+    }
+  }
+};
