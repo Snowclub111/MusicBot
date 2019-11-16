105d726410110e3f504926900f3ddff6d358e50c
Snowclub111 <57830261+Snowclub111@users.noreply.github.com>
2019-11-16 17:16:23 +0700

    Add files via upload

---
+++ Runbot.cmd
@@ -0,0 +1,7 @@
+@echo off
+title MusicBot | Bot By Snowclub
+color e
+:loop
+node index.js
+goto loop
+pause
---
+++ index.js
@@ -0,0 +1,55 @@
+const { CommandoClient } = require('discord.js-commando');
+const { Structures } = require('discord.js');
+const path = require('path');
+const { prefix, token } = require('./config.json');
+
+Structures.extend('Guild', Guild => {
+  class MusicGuild extends Guild {
+    constructor(client, data) {
+      super(client, data);
+      this.musicData = {
+        queue: [],
+        isPlaying: false,
+        nowPlaying: null,
+        songDispatcher: null
+      };
+      this.triviaData = {
+        isTriviaRunning: false,
+        wasTriviaEndCalled: false,
+        triviaQueue: [],
+        triviaScore: new Map()
+      };
+    }
+  }
+  return MusicGuild;
+});
+
+const client = new CommandoClient({
+  commandPrefix: prefix,
+  owner: '183647046564184065' // change this to your Discord user ID
+});
+
+client.registry
+  .registerDefaultTypes()
+  .registerGroups([
+    ['music', 'Music Command Group'],
+    ['gifs', 'Gif Command Group'],
+    ['other', 'random types of commands group'],
+    ['guild', 'guild related commands']
+  ])
+  .registerDefaultGroups()
+  .registerDefaultCommands()
+  .registerCommandsIn(path.join(__dirname, 'commands'));
+
+client.once('ready', () => {
+  console.log('Ready!');
+  client.user.setActivity('!help for commands', 'WATCHING');
+});
+
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find(c => c.name === 'general'); // change this to the channel you want to send the greeting to
+  if (!channel) return;
+  channel.send(`Welcome ${member}!`);
+});
+
+client.login(token);
